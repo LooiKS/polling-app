3ade948856ae74f47763de46ff906e06ac4fed1e
cheeyon97 <cheeyon97@hotmail.com>
2021-06-04 21:13:08 +0800

    for fix register error

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,25 +1,21 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {registerLocaleData} from "@angular/common";
-import {en_US, NZ_I18N} from 'ng-zorro-antd/i18n';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
 import en from '@angular/common/locales/en';
-import {SharedModule} from "./modules/shared/shared.module";
-import {CoreModule} from './modules/core/core.module';
-import {AccessTokenInterceptor} from "./modules/core/interceptor/AccessTokenInterceptor";
-
+import { SharedModule } from './modules/shared/shared.module';
+import { CoreModule } from './modules/core/core.module';
+import { AccessTokenInterceptor } from './modules/core/interceptor/AccessTokenInterceptor';
 registerLocaleData(en);
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     SharedModule,
     CoreModule,
@@ -31,14 +27,13 @@ registerLocaleData(en);
     ReactiveFormsModule,
   ],
   providers: [
-    {provide: NZ_I18N, useValue: en_US},
+    { provide: NZ_I18N, useValue: en_US },
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AccessTokenInterceptor,
-      multi: true
-    }
+      multi: true,
+    },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
