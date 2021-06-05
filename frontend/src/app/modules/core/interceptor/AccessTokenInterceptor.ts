0a63e12d48ea70a8b9f362b9c82f0a940ba3d67d
Kian Seng Looi <kianseng213@gmail.com>
2021-06-05 19:20:35 +0800

    1. add 401 & 403 handler in interceptor

--- frontend/src/app/modules/core/interceptor/AccessTokenInterceptor.ts
+++ frontend/src/app/modules/core/interceptor/AccessTokenInterceptor.ts
@@ -1,13 +1,16 @@
 import {Injectable} from "@angular/core";
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import { catchError } from "rxjs/operators";
+import { Router } from "@angular/router";
+import { NzMessageService } from "ng-zorro-antd/message";
 
 @Injectable()
 export class AccessTokenInterceptor implements HttpInterceptor {
 
   jwtToken: string;
 
-  constructor() {
+  constructor(private router: Router, private message: NzMessageService) {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -21,6 +24,15 @@ export class AccessTokenInterceptor implements HttpInterceptor {
       });
     }
     console.log(request);
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 403 || error.status === 401) {
+          // redirect to login page
+          this.message.error('Your login session is timed out.');
+          this.router.navigate(['/login']);
+        }
+        return throwError(error);
+      })
+    );
   }
 }
