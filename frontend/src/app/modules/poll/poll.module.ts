e6f4e9091a55f248edb951ff5b9dde4aabc7ebbf
cheeyon97 <cheeyon97@hotmail.com>
2021-06-06 14:55:25 +0800

    added tooltip text for poll

--- frontend/src/app/modules/poll/poll.module.ts
+++ frontend/src/app/modules/poll/poll.module.ts
@@ -14,6 +14,7 @@ import { NzAlertModule } from 'ng-zorro-antd/alert';
 import { NzProgressModule } from 'ng-zorro-antd/progress';
 import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
 import { AnswerPollPageComponent } from './page/answer-poll-page/answer-poll-page.component';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
 
 @NgModule({
   declarations: [
@@ -33,6 +34,7 @@ import { AnswerPollPageComponent } from './page/answer-poll-page/answer-poll-pag
     NzAlertModule,
     NzProgressModule,
     NzDescriptionsModule,
+    NzToolTipModule,
   ],
 })
 export class PollModule {}
